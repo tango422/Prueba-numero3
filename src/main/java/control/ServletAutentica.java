0ec040dea0671af59646414143f1d836e6f20c8b
unknown <paguilar3330@gmail.com>
2022-01-05 19:31:42 -0300

    first commit

---
+++ src/main/java/control/ServletAutentica.java
@@ -0,0 +1,40 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletAutentica
+ */
+@WebServlet("/ServletAutentica")
+public class ServletAutentica extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletAutentica() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1>INICIO DE SESION</h1>
+<form>
+	<table>
+	<tr>
+		<td>USUARIO:</td>
+		<td><input type="text" name="txtusuario"></td>
+	</tr>	
+	<tr>
+		<td>CONTRASEÑA:</td>
+		<td><input type="password" name="txtclave"></td>
+	</tr>	
+	
+	<tr>
+		
+		<td><input type="submit" name="ENVIAR"></td>
+	</tr>	
+	</table>
+
+</form>
+</body>
+</html>
